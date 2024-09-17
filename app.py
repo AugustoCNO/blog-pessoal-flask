c3c7367e6b9f6c3c3254891839a25181410ac5e5
AugustoCNO <gutocesar.c1@gmail.com>
2024-09-17 11:00:02 -0300

    reformulando banco de dados

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_login import current_user, login_required, login_user, logout_user, LoginManager
 from models.database import db
 from models.user import User
@@ -12,6 +12,7 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 db.init_app(app)
 login_manager.login_view = 'login'
+
 
 @login_manager.user_loader
 def load_user(user_id):
--- models/user.py
+++ models/user.py
@@ -4,8 +4,6 @@ from datetime import datetime
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), nullable=False, unique=True)
-    password = db.Column(db.String(80), nullable=False)
     title = db.Column(db.String(100), nullable=False)
     content = db.Column(db.Text, nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
